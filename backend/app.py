4d03b88ffd0a69b42adda65f4f03e37f35959dd3
primelaunch <primelaunch34v@gmail.com>
2025-02-28 13:50:23 -0500

    sss

--- backend/app.py
+++ backend/app.py
@@ -169,4 +169,4 @@ async def get_answers(request: Request, data: dict = Body(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0")
